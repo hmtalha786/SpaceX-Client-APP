a9b2b74adb709e94823693ef02a1dfd81bb8a594
Muhammad Talha <talhaofficialwork@gmail.com>
2021-01-10 19:29:57 +0500

    Footer Added

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -50,9 +50,12 @@ const Home: React.FC = () => {
                     </div>
                 </Fade>
             </div>
-
+            
             <img className="spacex-img" src="https://www.teslarati.com/wp-content/uploads/2019/06/Starlink-coverage-Earth-SpaceX-2-crop.jpg" alt="spacex-img" />
             
+            <div className="footer">
+			    <p>Privacy Policy | Terms and Conditions | SpaceX © 2020</p>
+		    </div>
         </div>
     )
 }
